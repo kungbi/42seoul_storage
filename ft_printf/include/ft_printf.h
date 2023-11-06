3bbcf3766ba0359e054502d8969d2f2d8fa1a7cd
woongbishin <kipper12335@gmail.com>
2023-11-07 00:59:58 +0900

    printf

---
+++ ft_printf/include/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: woonshin <woonshin@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 00:35:28 by woonshin          #+#    #+#             */
+/*   Updated: 2023/11/07 00:41:20 by woonshin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int ft_printf(const char *str, ...);
+
+#endif
