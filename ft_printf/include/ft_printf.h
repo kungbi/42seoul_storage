d00658969d60f1694d2ea2fa6a3ef8026068c58d
woongbishin <kipper12335@gmail.com>
2023-11-15 21:58:13 +0900

    push

--- ft_printf/include/ft_printf.h
+++ ft_printf/include/ft_printf.h
@@ -6,17 +6,21 @@
 /*   By: woonshin <woonshin@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 00:35:28 by woonshin          #+#    #+#             */
-/*   Updated: 2023/11/07 00:41:20 by woonshin         ###   ########.fr       */
+/*   Updated: 2023/11/09 08:58:52 by woonshin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+# define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-int ft_printf(const char *str, ...);
+int	ft_printf(const char *str, ...);
+/*
+  1. 글자에서 conversion specifier 찾는 함수
+  2. 각 conversion specifier 마다 파일 하나씩
+*/
 
 #endif
--- ft_printf/src/ft_printf.c
+++ ft_printf/src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: woonshin <woonshin@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 00:35:02 by woonshin          #+#    #+#             */
-/*   Updated: 2023/11/07 00:43:59 by woonshin         ###   ########.fr       */
+/*   Updated: 2023/11/15 10:09:52 by woonshin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int ft_printf(const char *str, ...)
 {
   char  *str_copy;
   
-  str_copy = (char *)str;
-  write(1, str_copy, 1);
+  ft_putstr_fd(str, 1);
   return (1);
 }
