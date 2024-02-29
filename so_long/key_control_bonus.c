e74ed51b4e51539c571fa05c0ec9b8b3ab58ce0a
Woongbi Shin <woonshin@c4r1s4.42seoul.kr>
2024-02-29 12:59:57 +0900

    so_long: final

---
+++ so_long/key_control_bonus.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_control.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: woonshin <woonshin@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/25 14:17:17 by woonshin          #+#    #+#             */
+/*   Updated: 2024/02/25 15:23:18 by woonshin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int	is_next_pos_available(t_game_info *game_info, t_pos *next_pos)
+{
+	if (game_info->map_info.map[next_pos->y][next_pos->x] == '1')
+		return (-1);
+	else if (game_info->map_info.map[next_pos->y][next_pos->x] == 'C')
+	{
+		game_info->map_info.map[next_pos->y][next_pos->x] = '0';
+		game_info->collection_cnt++;
+	}
+	else if (game_info->map_info.map[next_pos->y][next_pos->x] == 'E')
+	{
+		if ((size_t) game_info->collection_cnt
+			== game_info->map_info.objects.collection_cnt)
+		{
+			game_info->moved_cnt++;
+			return_ok();
+		}
+		return (-1);
+	}
+	return (1);
+}
+
+void	move(t_game_info *game_info, t_pos *next_pos)
+{
+	game_info->player_info.x = next_pos->x;
+	game_info->player_info.y = next_pos->y;
+	game_info->moved_cnt++;
+	ft_putnbr_fd(game_info->moved_cnt, 1);
+	ft_putstr_fd("\n", 1);
+}
+
+int	key_control(int keycode, t_game_info *game_info)
+{
+	t_pos	next_pos;
+
+	next_pos.x = game_info->player_info.x;
+	next_pos.y = game_info->player_info.y;
+	if (keycode == KEY_RIGHT)
+		next_pos.x++;
+	else if (keycode == KEY_LEFT)
+		next_pos.x--;
+	else if (keycode == KEY_UP)
+		next_pos.y--;
+	else if (keycode == KEY_DOWN)
+		next_pos.y++;
+	else if (keycode == KEY_ESC)
+	{
+		ft_putnbr_fd(game_info->moved_cnt, 1);
+		exit(0);
+	}
+	else
+		return (1);
+	if (keycode == KEY_LEFT || keycode == KEY_RIGHT)
+		game_info->player_info.dir = keycode;
+	if (is_next_pos_available(game_info, &next_pos) != 1)
+		return (0);
+	move(game_info, &next_pos);
+	return (0);
+}
---
+++ so_long/so_long_bonus.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: woonshin <woonshin@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/16 01:17:14 by woonshin          #+#    #+#             */
+/*   Updated: 2024/02/27 14:34:25 by woonshin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int	main(int argc, char *argv[])
+{
+	t_game_info	game_info;
+
+	if (argc != 2)
+		return_error();
+	if (ft_strncmp(&argv[1][ft_strlen(argv[1]) - 4], ".ber", 4) != 0)
+		return_error();
+	map_info_init(&game_info.map_info);
+	map_input(&game_info.map_info, argv[1]);
+	map_check(&game_info.map_info);
+	game_info_init(&game_info);
+	create_enemy(&game_info);
+	mlx_key_hook(game_info.win, key_control, &game_info);
+	mlx_loop_hook(game_info.mlx, render, &game_info);
+	mlx_hook(game_info.win, 17, 0, on_destroy, &game_info);
+	mlx_loop(game_info.mlx);
+}
+
+void	map_info_init(t_map_info *map_info)
+{
+	map_info->objects.collection_cnt = 0;
+	map_info->objects.exit_cnt = 0;
+	map_info->objects.player_cnt = 0;
+}
+
+int	is_collision(t_game_info *game_info)
+{
+	int	i;
+
+	i = 0;
+	while (i < game_info->enemy_cnt)
+	{
+		if (game_info->enemy_info[i].x == game_info->player_info.x
+			&& game_info->enemy_info[i].y == game_info->player_info.y)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	get_frame(int x)
+{
+	if (15 <= x)
+		return (1);
+	return (15 - x);
+}
