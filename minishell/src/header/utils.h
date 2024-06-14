c6105a15408b1369fb099d2bbd1d5665bf9e2739
Woongbi Shin <woonshin@c1r3s4.42seoul.kr>
2024-06-15 06:56:48 +0900

    minishit: 폴더 정리

--- minishell/src/header/utils.h
+++ minishell/src/header/utils.h
@@ -1,27 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: woonshin <woonshin@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/15 00:35:44 by woonshin          #+#    #+#             */
-/*   Updated: 2024/06/15 06:46:36 by woonshin         ###   ########.fr       */
+/*   Created: 2024/06/15 06:42:41 by woonshin          #+#    #+#             */
+/*   Updated: 2024/06/15 06:46:54 by woonshin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-# include <stdio.h>
-# include <readline/readline.h>
-# include "parse.h"
-# include <stdlib.h>
 
-typedef struct s_env
-{
-	char	*data;
-	char	*key;
-	char	*value;
-}	t_env;
+#ifndef UTILS_H
+# define UTILS_H
+# include "libft.h"
+# include "minishell.h"
+
+
+char	*expand_env(t_list *env_list, char *origin);
 
 #endif
