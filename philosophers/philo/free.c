5066b57259e9c0efcc9063b01c266279c0a75bf1
kungbi <kipper12335@gmail.com>
2024-09-09 13:20:27 +0900

    🐛 Fix: mutex destroy 추가

--- philosophers/philo/free.c
+++ philosophers/philo/free.c
@@ -6,7 +6,7 @@
 /*   By: woonshin <woonshin@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 15:27:08 by woonshin          #+#    #+#             */
-/*   Updated: 2024/09/04 16:44:37 by woonshin         ###   ########.fr       */
+/*   Updated: 2024/09/09 13:19:22 by woonshin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,12 @@ void	free_system(t_system *system)
 	while (i < system->args.philo_num)
 	{
 		pthread_mutex_destroy(&system->forks[i]);
+		pthread_mutex_destroy(&system->philos[i].eat_mutex);
 		i++;
 	}
+	pthread_mutex_destroy(&system->stop_mutex);
+	pthread_mutex_destroy(&system->print_mutex);
+	pthread_mutex_destroy(&system->full_mutex);
 	free(system->philos);
 	free(system->forks);
 	free(system->threads);
--- philosophers/philo/header/libft.h
+++ philosophers/philo/header/libft.h
@@ -6,7 +6,7 @@
 /*   By: woonshin <woonshin@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 10:57:56 by woonshin          #+#    #+#             */
-/*   Updated: 2024/09/08 11:02:20 by woonshin         ###   ########.fr       */
+/*   Updated: 2024/09/09 13:10:49 by woonshin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 # define LIBFT_H
 # include <stdio.h>
 
-int	ft_atoi(const char *str);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int	ft_isdigit(int c);
+int		ft_atoi(const char *str);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_isdigit(int c);
 size_t	ft_strlen(const char *s);
 
 #endif
