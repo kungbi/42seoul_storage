216e40a532bae45d727e39ede10071567b6f81bd
kungbi <kipper12335@gmail.com>
2025-02-15 13:54:18 +0900

    main 생성

--- cpp/module_05/ex03/main.cpp
+++ cpp/module_05/ex03/main.cpp
@@ -6,40 +6,29 @@
 #include "Intern.hpp"
 
 int main(void) {
+	Intern intern;
+
+
+	std::cout << "================ Shrubbery ================\n";
+	AForm* scf = intern.makeForm("shrubbery creation", "42seoul");
+	std::cout << *scf << "\n";
+	delete scf;
+
+	std::cout << "\n================ Robotomy ================\n";
+	AForm *rrfe = intern.makeForm("robotomy request", "42seoul");
+	std::cout << *rrfe << "\n";
+	delete rrfe;
+
+	std::cout << "\n================ Presidential ================\n";
+	AForm *ppf = intern.makeForm("presidential pardon", "42seoul");
+	std::cout << *ppf << "\n";
+	delete ppf;
 
 	try {
-		Intern intern;
-		AForm* scf = intern.makeForm("shrubbery creation", "42seoul");
-		std::cout << *scf << "\n";
-		delete scf;
-		std::cout << "================ Shrubbery ================\n";
-
-		AForm *rrfe = intern.makeForm("robotomy request", "42seoul");
-		std::cout << *rrfe << "\n";
-		delete rrfe;
-		std::cout << "\n================ Robotomy ================\n";
-
-		AForm *ppf = intern.makeForm("presidential pardon", "42seoul");
-		std::cout << *ppf << "\n";
-		delete ppf;
-		std::cout << "\n================ Presidential ================\n";
-
+		std::cout << "\n================ Unknown ================\n";
 		AForm *unknown = intern.makeForm("Unknown", "42seoul");
 		std::cout << unknown << "\n";
 		delete unknown;
-		std::cout << "\n================ Unknown ================\n";
-
-		{
-			Intern someRandomIntern;
-			AForm* rrf;
-
-			rrf = someRandomIntern.makeForm("robotomy request" , "Bender");
-			std::cout << *rrf << "\n";
-			delete rrf;
-
-		}
-		std::cout << "\n================ Main ================\n";
-
 	} catch (const std::exception &e) {
 		std::cout << e.what() << "\n";
 	}
