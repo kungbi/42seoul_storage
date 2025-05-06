88b2eabd4a25a3d21ebc2b3038555f2b265ef87a
kungbi <kipper12335@gmail.com>
2025-05-06 22:51:23 +0900

    Add RPN class implementation with basic arithmetic operations and error handling

---
+++ cpp/module_09/ex01/RPN.hpp
@@ -0,0 +1,40 @@
+#ifndef RPN_HPP
+# define RPN_HPP
+
+#include <iostream>
+#include <stack>
+#include <cstdlib>
+
+#define number 1
+#define operand 2
+
+enum {
+	NUMBER,
+	OPERATOR,
+	INVALID
+};
+
+class RPN 
+{
+	private: 
+		std::stack<int> stack;
+		std::string math_str;
+
+		bool isValidExpression(const std::string &expr);
+		void showMessage(const std::string &msg);
+	public:
+		RPN();
+		RPN(const char *argv);
+		~RPN();
+		RPN&operator=(RPN const &rpn);
+		RPN(RPN const &rpn);
+
+
+		int checkValue(char math);
+		int operand_calc(char op, int num1, int num2);
+		int takePop();
+		int handleError(std::string math);
+		int calcRpn(void);
+};
+
+#endif
